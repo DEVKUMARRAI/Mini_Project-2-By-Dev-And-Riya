865ffe3f1058a5922fa04137a391d47797033720
Dev <devrai197551@gmail.com>
2023-04-29 19:42:49 +0530

    css finalised

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import Logo from '../assets/images/logofinal.jpeg';
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '100px', height: '100px', margin: '0px 20px' }} />
+      <img src={Logo} alt="logo" style={{ width: '135px', height: '120px', margin: '0px 20px' }} />
     </Link>
     <Stack
       direction="row"
